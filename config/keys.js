7e79f023423b04a4cc95c10cc11f910329854888
morningbob <hon898@gmail.com>
2019-01-21 15:41:29 -0500

    corrected keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 // keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
 	// we are in production - return the prod set of keys
-	require('./prod.js');
+	module.exports = require('./prod.js');
 } else {
 	// we are in development - return the dev keys
-	require('./dev.js');
+	module.exports = require('./dev.js');
 }
